50cc58b13388c3603b9bbfd41d82be90c780d589
diracsportech0 <dirac.sportech@gmail.com>
2025-12-03 11:12:58 -0500

    corregir filtrado por TIpo y por Tercio v2.2

--- pages/4_⚽Analisis.py
+++ pages/4_⚽Analisis.py
@@ -16,7 +16,7 @@ from streamlit_plotly_events import plotly_events
 from PIL import Image
 #url_powerbi = '<iframe title="Plataforma Dirac v1.1" width="900" height="500" src="https://app.powerbi.com/view?r=eyJrIjoiOWM0YmNkMGEtMzc4Ni00MTI4LTk0OGEtZmFhNzc5NTZiYTkxIiwidCI6IjBlMGNiMDYwLTA5YWQtNDlmNS1hMDA1LTY4YjliNDlhYTFmNiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>'
 #st.markdown(url_powerbi, unsafe_allow_html=True)
-
+st.write(df)
 #----------------------
 
 #st.title(f'⚽ {name_club}')
