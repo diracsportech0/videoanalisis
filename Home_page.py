79bc023b0539c63412ddd1b32e9d52c199752e47
diracsportech0 <dirac.sportech@gmail.com>
2025-12-04 13:31:21 -0500

    bienvenido DT

--- Home_page.py
+++ Home_page.py
@@ -7,7 +7,7 @@ st.set_page_config(layout="wide")
 # ENCABEZADO: escudo Melgar + escudo Liga1
 colA, colB, colC = st.columns([1, 7, 1])
 with colA:
-    st.image('logo-equipo.png', use_column_width=True)
+    st.image('logo-dirac.png', use_column_width=True)
 with colB:
     pass
 with colC:
@@ -15,7 +15,7 @@ with colC:
     pass
 
 #FORMATO
-st.header(f'Bienvenido, DT cCesar Salas!!')
+st.header(f'Bienvenido, DT Cesar Salas!!')
 
 
 
